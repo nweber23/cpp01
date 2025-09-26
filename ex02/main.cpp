5544a1df7ddb6e617019a7e8f755acf7b8bbc4b9
Niklas Weber <nweber@student.42Heilbronn.de>
2025-09-26 21:55:48 +0200

    ex02 finished

---
+++ ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string s = "HI THIS IS BRAIN";
+	std::string* stringPTR = &s;
+	std::string& stringREF = s;
+
+	std::cout << &s << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << s << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+	return 0;
+}
